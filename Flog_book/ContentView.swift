7e3f87f3a4831d3b9d0190156d1eb2b52167bd27
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-10-14 20:48:14 +0900

    first commit with .gitignore

---
+++ Flog_book/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flog_book
+//
+//  Created by 山本裕太 on 2019/10/14.
+//  Copyright © 2019 山本裕太. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
